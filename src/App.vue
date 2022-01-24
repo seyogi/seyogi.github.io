03f8ac87fef25ef8eff0facd571a3e661ff60c3a
seyogi <41766832+seyogi@users.noreply.github.com>
2022-01-24 16:02:31 +0900

    from vscord

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,65 @@
   <div>
     <Header></Header>
     <Banner></Banner>
-    <p v-if="msg.length > 0">
-      {{ msg }}
-    </p>
-    <p v-else>no text</p>
-    <input type="text" v-model="msg" />
+    <div class="container">
+      <Main class="center_container"></Main>
+      <Twitter class="right_container"></Twitter>
+    </div>
   </div>
 </template>
 
 <script>
 import Header from "./components/Header";
 import Banner from "./components/Banner";
+import Main from "./components/Main";
+import Twitter from "./components/Twitter";
 
 export default {
   components: {
     Header,
     Banner,
-  },
-  data() {
-    return {
-      msg: "ホームページ更新中です",
-    };
+    Main,
+    Twitter,
   },
 };
 </script>
+
+<style scoped>
+/*---------- Banner ----------*/
+@media screen and (max-width: 1379px){
+    .center_container {
+        margin-left: auto;
+        margin-right: auto;
+        width: 940px;
+    }
+    .right_container {
+        display:none ;
+        width: 0%;
+    }
+}
+@media screen and (min-width: 1380px){
+  .container {
+    max-width: 1380px; /*必要な幅に設定*/
+    margin: auto; /*ブラウザ中央に配置*/
+  }
+  .container:after { /*clearfix設定*/
+    content:"";
+    display:block;
+    clear:both;
+  }
+  .center_container {
+    width: 940px;
+    padding-left: 220px;
+    float: left; /*左にフロート配置*/
+  }
+  .right_container {
+    height: 100%;
+    display: inline;
+    float: right;
+    position: sticky;
+    top: 20px;
+  }
+}
+
+</style>
+
---
+++ src/components/Main.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p v-if="msg.length > 0">
+      {{ msg }}
+    </p>
+    <p v-else>no text</p>
+    <input type="text" v-model="msg" />
+    <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br><br> <br> <br> <br> <br> <br> <br><br> <br> <br> <br> <br> <br> <br><br> <br> <br><br> <br> <br> <br> <br> <br> <br>
+    音楽ゲームを嗜む情報系(弱い)学生です。<br>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  data() {
+    return {
+      msg: "ホームページ更新中です",
+    };
+  },
+};
+</script>
