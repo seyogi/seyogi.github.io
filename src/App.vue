7eea1511926505cb50abb345c7c37855ef140344
seyogi <41766832+seyogi@users.noreply.github.com>
2022-01-31 22:45:58 +0900

    こみっと

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,14 @@
       <Main class="center_container"></Main>
       <Twitter class="right_container"></Twitter>
     </div>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Header from "./components/Header";
 import Banner from "./components/Banner";
+import Footer from "./components/Footer";
 import Main from "./components/Main";
 import Twitter from "./components/Twitter";
 
@@ -21,32 +23,35 @@ export default {
     Banner,
     Main,
     Twitter,
+    Footer,
   },
 };
 </script>
 
 <style scoped>
+
 /*---------- Banner ----------*/
-@media screen and (max-width: 1379px){
-    .center_container {
-        margin-left: auto;
-        margin-right: auto;
-        width: 940px;
-    }
-    .right_container {
-        display:none ;
-        width: 0%;
-    }
+@media screen and (max-width: 1379px) {
+  .center_container {
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 940px;
+  }
+  .right_container {
+    display: none;
+    width: 0%;
+  }
 }
-@media screen and (min-width: 1380px){
+@media screen and (min-width: 1380px) {
   .container {
     max-width: 1380px; /*必要な幅に設定*/
     margin: auto; /*ブラウザ中央に配置*/
   }
-  .container:after { /*clearfix設定*/
-    content:"";
-    display:block;
-    clear:both;
+  .container:after {
+    /*clearfix設定*/
+    content: "";
+    display: block;
+    clear: both;
   }
   .center_container {
     width: 940px;
@@ -54,6 +59,7 @@ export default {
     float: left; /*左にフロート配置*/
   }
   .right_container {
+    padding-top: 20px;
     height: 100%;
     display: inline;
     float: right;
@@ -61,6 +67,4 @@ export default {
     top: 20px;
   }
 }
-
 </style>
-
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
-    <p v-if="msg.length > 0">
-      {{ msg }}
-    </p>
-    <p v-else>no text</p>
-    <input type="text" v-model="msg" />
-    <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br><br> <br> <br> <br> <br> <br> <br><br> <br> <br> <br> <br> <br> <br><br> <br> <br><br> <br> <br> <br> <br> <br> <br>
-    音楽ゲームを嗜む情報系(弱い)学生です。<br>
+    <div class="main_box">
+      <br> 
+      一般学生音ゲーマーによる雑記ページです。<br>
+      Vuetify触ったし年内にはサイト一新しようかなーとか考えてます。<br>
+      <br>
+      <p v-if="msg.length > 0">
+        {{ msg }}
+      </p>
+      <p v-else>no text</p>
+      <input type="text" v-model="msg" /><br>
+      <br>
+      音ゲー自語りページ : 閉鎖中<br>
+      <br> <br> <br> <br> <br>  <br> <br> <br><br> <br> <br><br> <br> <br> <br> <br> <br> <br>
+      <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
+    </div>
   </div>
 </template>
 
@@ -21,3 +29,12 @@ export default {
   },
 };
 </script>
+
+
+<style scoped>
+.main_box {
+
+    margin-left : 10px ;
+}
+
+</style>
