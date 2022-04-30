bc16d85ddb13525fe73ef5e3a9c922be6dc0b894
seyogi <41766832+seyogi@users.noreply.github.com>
2022-04-30 23:29:34 +0900

    hanicamu

--- src/components/Rhythmparts/Selector.vue
+++ src/components/Rhythmparts/Selector.vue
@@ -2,8 +2,8 @@
   <div>
     <div class="main_box">
       <div style="display: flex">
-        <div v-for="game in games" :key="game.title">
-          <div class="detail_hexagon" :style="game.styles"></div>
+        <div v-for="game in games" :key="game.index">
+          <div class="detail_hexagon" :style="position(game)"></div>
         </div>
       </div>
     </div>
@@ -16,36 +16,63 @@ export default {
     return {
       games: [
         {
+          index: 0,
           title: "オンゲキ",
-          styles: {
-            //http://alphasis.info/wp-content/uploads/2012/01/gimp-tutorial-12012402.jpg
-            "background-image": `url(/img/Rhythm/logo/ongeki_logo.jpg)`,
-          },
+          image: `url(/img/Rhythm/logo/ongeki_logo.png)`,
+          color: "pink",
         },
         {
-          title: "Test",
-          styles: {
-            "background-image": `url(http://alphasis.info/wp-content/uploads/2012/01/gimp-tutorial-12012402.jpg)`,
-          },
+          index: 1,
+          title: "CHUNITHM",
+          image: `url(/img/Rhythm/logo/chunithm_logo.png)`,
+          color: "#FF6",
+        },
+        {
+          index: 2,
+          title: "SDVX",
+          image: `url(/img/Rhythm/logo/sdvx_logo.png)`,
+          color: "#E6E6E6",
+        },
+        {
+          index: 3,
+          title: "ARCAEA",
+          image: `url(/img/Rhythm/logo/arcaea_logo.png)`,
+          color: "#1f1e33",
         },
       ],
     };
+  },
+  computed: {
+    position: function () {
+      return function (data) {
+        const num = 80;
+        var a = {
+          "top":num*(data.index%2)+"px",
+          "left":Math.sqrt(3)*num*data.index+"px",
+          "background-image": data.image,
+          "background-color":data.color,
+        };
+        return a;
+      };
+    },
   },
 };
 </script>
 
 <style scoped>
 .main_box {
+  height: 280px;
+  position: relative;
   margin-left: 10px;
 }
 .detail_hexagon {
-  display: block;
-  width: 200px;
-  height: 173px;
+  padding: 10px;
+  cursor: pointer;
+  width: 160px;
+  height: 138px;
   clip-path: polygon(25% 0, 75% 0, 100% 50%, 75% 100%, 25% 100%, 0% 50%);
   -webkit-clip-path: polygon(25% 0, 75% 0, 100% 50%, 75% 100%, 25% 100%, 0% 50%);
-  position: relative;
-  overflow: hidden;
+  position: absolute;
   background-size: contain;
   background-position: center;
 }
