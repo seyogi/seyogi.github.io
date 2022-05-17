cb3c9e1dd0df48a1a2fc6d5b1723aaddda234b90
seyogi <41766832+seyogi@users.noreply.github.com>
2022-05-17 10:57:23 +0900

    ばんきしゃ

--- src/components/Rhythmparts/CHUNITHM.vue
+++ src/components/Rhythmparts/CHUNITHM.vue
@@ -5,7 +5,7 @@
       <img src="/img/Rhythm/Result/UNI01.jpg" class="img" />
       <img src="/img/Rhythm/Result/UNI02.jpg" class="img" /><br />
       <br />
-      Rating : 17.21(NEW)<br />
+      Rating : 17.25(NEW)<br />
       CLASS EMBLEM : INFINITY -ALL CLEAR-<br>
       <br />
       15全鳥<br />
