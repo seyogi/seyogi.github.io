b27fe9178db2b3f4fe79b28f54876a819f44ed51
Seyogi <tai.chiba.74@gmail.com>
2025-01-11 21:04:59 +0900

    delete SCIIDX

--- src/router.js
+++ src/router.js
@@ -2,7 +2,6 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeBody from './views/HomeBody.vue'
 import RhythmBody from './views/RhythmBody.vue'
 import iidxRadarBody from './views/iidxRadarBody.vue'
-import iidxScrapingBody from './components/iidxScrapingparts/iidxScrapingBody.vue' 
 import ContactBody from './views/ContactBody.vue'
 
 const routes = [
@@ -20,11 +19,6 @@ const routes = [
     path: '/iidx_radar',
     name: 'iidx_radar',
     component: iidxRadarBody
-  },
-  {
-    path: '/iidx_Scraping',
-    name: 'iidx_Scraping',
-    component: iidxScrapingBody
   },
   {
     path: '/Contact',
