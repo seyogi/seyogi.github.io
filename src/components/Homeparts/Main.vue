0b405db0c437d0e97c5c3023cb35a75e63532139
Seyogi <tai.chiba.74@gmail.com>
2025-04-05 22:28:37 +0900

    サオリ

--- src/components/Homeparts/Main.vue
+++ src/components/Homeparts/Main.vue
@@ -4,16 +4,54 @@
       <br />
       <h2>Top</h2><br />
       <div class="intro_box">
-        Aboutを分離したらトップページが寂しくなってしまいました；；<br />
-        何かいいアイデアを思いついたら更新します<br />
+        Aboutを分離したらトップページが寂しくなってしまったので、以下仮置き<br /><br />
+        <h3>サオリさん絆ランクログ</h3>
+        <hr style="width: 100%" />
+        <a class="date_box">2024.7</a>: お出迎え Sheside outside 神イベでした...<br>
+        <a class="date_box">2024.9</a>: 絆50 9月3日は錠前サオリさんの誕生日です<br>
+        <a class="date_box">2024.11</a>: 絆60<br>
+        <a class="date_box">2025.1</a>: 絆70 だいたい1/3 先は長い<br>
+        <a class="date_box">2025.4</a>:<br>
+        <img class="saoriImg" src="/img/saori78.png" /><br><br>
       </div>
+     
     </div>
   </div>
 </template>
 
+
+<script>
+import TimelineContents from '../../assets/SaoriKizuna.json';
+import Timeline from "./Timeline.vue";
+
+export default {
+  components: {
+    Timeline,
+  },
+  data() {
+    return {
+      timeline_contents:TimelineContents,
+    };
+  },
+};
+</script>
+
+
 <style scoped>
 
-.intro_box{
+  .saoriImg {
+    width: 90%;
+    max-width: 600px;
+    margin: auto;
+    display: block;
+  }
+  .date_box{
+    display: inline-block;
+    min-width:80px;
+    margin-left: 20px;
+  } 
+
+  .intro_box{
     margin-left: 20px;
   } 
 
---
+++ src/components/Homeparts/Timeline.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="timeline" v-for="content in contents" :key="content">
+    <li>
+      <div class="timeline-line" />
+      <p class="timeline-date" v-if="'date_y' in content">
+        <a v-if="'date_y' in content">{{ content.date_y }} </a>
+        <a class="month" v-if="'date_m' in content">{{ content.date_m }}</a>
+      </p>
+
+      <div class="timeline-content">
+        <div class="timeline-circle" v-if="content.circle == true" />
+        <div class="timeline-txt">
+          <div v-if="content.bold == true">
+            <h3>{{ content.main_text }}</h3>
+          </div>
+          <div v-if="content.bold != true">
+            {{ content.main_text }}
+          </div>
+          <div class="sub-txt" v-if="'sub_text' in content">
+            {{ content.sub_text }}
+          </div>
+        </div>
+      </div>
+    </li>
+  </div>
+</template>
+  
+  <script>
+  
+  export default {
+    props: ['timeline_contents'],
+    data() {
+      return {
+          contents:this.timeline_contents,
+      };
+    },
+  };
+  </script>
+  
+<style scoped>
+.main_box {
+  padding: 30px;
+}
+.timeline {
+  list-style: none;
+}
+
+@media screen and (max-width: 700px) {
+  .timeline > li {
+    overflow: hidden;
+    position: relative;
+    margin: 0;
+  }
+  .timeline-line {
+    content: "";
+    width: 3px;
+    height: 100%;
+    background: #888;
+    position: absolute;
+    left: 20px;
+  }
+  .timeline-circle {
+    content: "";
+    width: 12px;
+    height: 12px;
+    background: #833;
+    position: absolute;
+    left: 16px;
+    top: 5px;
+    border-radius: 100%;
+  }
+  .timeline-content {
+    width: 85%;
+    float: left;
+    position: relative;
+    padding-left: 50px;
+  }
+  .timeline-date {
+    width: 80px;
+    margin: 4px;
+    text-align: center;
+    background: #daa;
+    border-radius: 10px; /* 角丸半径を50%にする(=円形にする) */
+    margin-left: 40px;
+  }
+  .timeline-date .month {
+    display: none;
+  }
+  .timeline-txt{
+    font-size: 15px;
+    margin-bottom: 15px;
+  }
+  .timeline-txt .sub-txt {
+    font-size: 12px;
+  }
+}
+/* for Desktop */
+@media screen and (min-width: 700px) {
+  .timeline > li {
+    overflow: hidden;
+    position: relative;
+    margin: 0;
+  }
+  .timeline-date {
+    width: 100px;
+    float: left;
+    margin-top: 12px;
+    margin-right: 10px;
+    text-align: right;
+  }
+  .timeline-line {
+    content: "";
+    width: 3px;
+    height: 100%;
+    background: #888;
+    position: absolute;
+    left: 110px;
+  }
+  .timeline-circle {
+    content: "";
+    width: 12px;
+    height: 12px;
+    background: #833;
+    position: absolute;
+    left: 106px;
+    top: 16px;
+    border-radius: 100%;
+  }
+  .timeline-content {
+    width: 75%;
+    float: left;
+    padding-left: 30px;
+  }
+  .timeline-txt {
+    font-size: 18px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+  .timeline-txt .sub-txt {
+    font-size: 12px;
+  }
+}
+</style>
