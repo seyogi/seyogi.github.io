267c4874aad2576eaaedf97f56a17c150747e437
Seyogi <tai.chiba.74@gmail.com>
2025-05-09 16:41:58 +0900

    info box 追加

--- src/components/Aboutparts/ActivityTimeline.vue
+++ src/components/Aboutparts/ActivityTimeline.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="timeline" v-for="content in contents" :key="content">
+  <div class="timeline" v-for="(content, index) in contents" :key="index">
     <li>
       <div class="timeline-line" />
       <p class="timeline-date" v-if="'date_y' in content">
@@ -8,40 +8,67 @@
 
       <div class="timeline-content">
         <div class="timeline-circle" v-if="content.circle == true" />
-        <div class="timeline-txt">
-          <div v-if="content.circle == true">
-            <a :href="content.url" target="_blank">
-              <font-awesome-icon class="icons" icon="fa-solid fa-school fa-xl" color="#422"/>
-            </a>
+        <div class="main-block">
+          <div class="timeline-txt">
+            <div v-if="content.circle == true">
+              <a :href="content.url" target="_blank">
+                <font-awesome-icon class="icons" icon="fa-solid fa-school" color="#422"/>
+              </a>
+            </div>
+            <div class="main-txt">
+              {{ content.main_text }}
+              <a v-if="'info' in content" value="info" @click="click(index)">
+                <font-awesome-icon class="icons" icon="fa-solid fa-circle-info" color="#422"/>
+              </a>
+
+            </div>
+            <div class="sub-txt" v-if="'product' in content">
+              {{ content.product }}
+            </div>
+            <div class="sub-txt" v-if="'team_name' in content">
+              Team : {{ content.team_name }}
+            </div>
+            <div class="sub-txt" v-if="'achievements_text' in content">
+              Award: {{ content.achievements_text }}
+            </div>
           </div>
-          <div class="main-txt">
-            {{ content.main_text }}
-          </div>
-          <div class="sub-txt" v-if="'product' in content">
-            {{ content.product }}
-          </div>
-          <div class="sub-txt" v-if="'team_name' in content">
-            Team : {{ content.team_name }}
-          </div>
-          <div class="sub-txt" v-if="'achievements_text' in content">
-            Award: {{ content.achievements_text }}
-          </div>
+          <img v-if="index != openIndex" class="side-img" :src="content.imgurl" />
         </div>
-        <img class="img" :src="content.imgurl" />
+        <div >
+          <transition name="collapse">
+            <div v-show="index === openIndex" class="info_wrapper">
+              <transition name="fade" appear>
+                <div v-if="index === openIndex" class="info-block">
+                  <img :src="content.imgurl" class="info-img" />
+                  <div class="info-txt">
+                     {{ content.info }}
+                  </div>
+                </div>
+              </transition>
+            </div>
+          </transition>
+        </div>
       </div>
     </li>
   </div>
 </template>
 
 <script>
+import '@/components/Aboutparts/info.css';
 
 export default {
   props: ['timeline_contents'],
   data() {
     return {
         contents:this.timeline_contents,
+        openIndex: null
     };
   },
+  methods: {
+    click(index) {
+      this.openIndex = this.openIndex === index ? null : index;
+    },
+  }
 };
 </script>
 
@@ -90,18 +117,36 @@ export default {
   .timeline-date .month {
     display: none;
   }
-  .timeline-txt{
+  .timeline-content .main-block .timeline-txt{
     font-size: 15px;
     margin-bottom: 15px;
   }
-  .timeline-txt .main-txt{
+  .timeline-content .main-block .timeline-txt .main-txt{
     font-weight: bold;
   }
-  .timeline-txt .sub-txt {
+  .timeline-content .main-block .timeline-txt .sub-txt {
     font-size: 12px;
   }
-  .timeline img {
+  .timeline-content .main-block .side-img {
     display: none;
+  }
+  .timeline-content .info-block{
+    padding-left: 20px;
+    padding-right: 20px;
+    display: grid;
+    place-items: center;
+  }
+  .timeline-content .info-block .info-img{
+    max-width: 100%;
+    max-height: 300px;
+  }
+  .timeline-content .info-block .info-txt {
+    padding: 10px;
+    font-size: 14px;
+    line-height: 2.0;
+    white-space: pre-wrap;
+    font-family: monospace;
+    tab-size: 0;
   }
 }
 /* for Desktop */
@@ -139,25 +184,45 @@ export default {
   .timeline-content {
     width: uv(100%-100px);
     margin-left: 100px;
+  }
+  .timeline-content .main-block{
     display:flex;
     justify-content: space-between;
   }
-  .timeline-txt {
+  .timeline-content .main-block .timeline-txt {
     font-size: 17px;
     margin-top: 10px;
     margin-bottom: 10px;
     max-width: 450px;
   }
-  .timeline-txt .main-txt{
+  .timeline-content .main-block .timeline-txt .main-txt{
     font-weight: 600;
   }
-  .timeline-txt .sub-txt {
+  .timeline-content .main-block .timeline-txt  .sub-txt {
     font-size: 13px;
   }
-  .timeline-content img {
+  .timeline-content .main-block .side-img {
     max-width: 100%;
     max-height: 100px;
     margin-top: 10px;
+  }
+  .timeline-content .info-block{
+    padding-left: 20px;
+    padding-right: 20px;
+    display: grid;
+    place-items: center;
+  }
+  .timeline-content .info-block .info-img{
+    max-width: 100%;
+    max-height: 300px;
+  }
+  .timeline-content .info-block .info-txt {
+    padding: 10px;
+    font-size: 14px;
+    line-height: 2.0;
+    white-space: pre-wrap;
+    font-family: monospace;
+    tab-size: 0;
   }
 }
 </style>
