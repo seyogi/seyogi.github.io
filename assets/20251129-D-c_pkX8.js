d6222280ea38aabd52bcca0805167a3db7ecdc85
Seyogi <tai.chiba.74@gmail.com>
2025-11-30 03:05:18 +0900

    Updates

---
+++ assets/20251129-D-c_pkX8.js
@@ -0,0 +1,12 @@
+const n=`---\r
+title: "初めまして"\r
+date: 2025-11-29\r
+tags: ["tips", "vue", "github-pages"]\r
+---\r
+\r
+個人ホームページでブログを更新するという古の文化に憧れがあったので、不定期に更新していこうと思います。\r
+\r
+よろしく～\r
+\r
+\r
+`;export{n as default};
